9165ce3b3ba2c53a54b22e262549a8a7934507c8
Kenta OONO <oono@preferred.jp>
2015-12-30 12:18:53 +0900

    Refactor

--- toy.py
+++ toy.py
@@ -29,60 +29,66 @@ def calc_ab(eps_start, eps_end, gamma, epoch):
     return A, B
 
 
+# data generation
 THETA1 = 0
 THETA2 = 1
 VAR1 = 10
 VAR2 = 1
 VAR_X = 2
+
+# SGLD parameters
 EPS_START = 0.01
 EPS_END = 0.0001
 GAMMA = 0.55
 EPOCH = 100
 A, B = calc_ab(EPS_START, EPS_END, GAMMA, EPOCH)
+
+# sample size
 n = 100
 batchsize = 1
-n_batchsize = (n + batchsize - 1) // batchsize
+
 SEED = 0
 numpy.random.seed(SEED)
 
-def update(theta1, theta2, x, epoch):
-    theta1 = chainer.Variable(numpy.array(theta1, dtype=numpy.float32))
-    theta2 = chainer.Variable(numpy.array(theta2, dtype=numpy.float32))
 
+def calc_log_posterior(theta, x):
+    theta1, theta2 = F.split_axis(theta, 2, 0)
     log_prior1 = F.sum(F.log(gaussian_likelihood(theta1, 0, VAR1)))
     log_prior2 = F.sum(F.log(gaussian_likelihood(theta2, 0, VAR2)))
     prob1 = gaussian_likelihood(x, theta1, VAR_X)
     prob2 = gaussian_likelihood(x, theta1 + theta2, VAR_X)
     log_likelihood = F.sum(F.log(prob1 / 2 + prob2 / 2))
-    log_posterior = log_prior1 + log_prior2 + log_likelihood * n / len(x)
+    return log_prior1 + log_prior2 + log_likelihood * n / len(x)
 
-    theta1.zerograd()
-    theta2.zerograd()
-    log_posterior.backward()
 
+def calc_grad(theta, x)
+
+def update(theta, epoch):
     eps = A / (B + epoch) ** GAMMA
     eta = numpy.random.randn() * numpy.sqrt(eps)
-    d_theta1 = theta1.grad[0] * eps / 2 + eta
-    d_theta2 = theta2.grad[0] * eps / 2 + eta
-    return d_theta1, d_theta2
+    d_theta = theta.grad * eps / 2 + eta
+    return theta.data + d_theta
 
 
-theta1 = numpy.random.randn(1) * numpy.sqrt(VAR1)
-theta2 = numpy.random.randn(1) * numpy.sqrt(VAR2)
 theta1_all = numpy.empty((EPOCH * n,), dtype=numpy.float32)
 theta2_all = numpy.empty((EPOCH * n,), dtype=numpy.float32)
+theta = numpy.random.randn(2) * [numpy.sqrt(VAR1), numpy.sqrt(VAR2)]
 x = generate(n, THETA1, THETA2, VAR_X)
 for epoch in six.moves.range(EPOCH):
     perm = numpy.random.permutation(n)
     for i in six.moves.range(0, n, batchsize):
-        d_theta1, d_theta2 = update(
-            theta1, theta2, x[perm][i: i+batchsize], epoch)
-        theta1 += d_theta1
-        theta2 += d_theta2
-        theta1_all[epoch * n + i / batchsize] = theta1[0]
-        theta2_all[epoch * n + i / batchsize] = theta2[0]
+        theta = chainer.Variable(numpy.array(theta, dtype=numpy.float32))
+        log_posterior = calc_log_posterior(theta, x[perm][i: i+batchsize])
+
+        theta.zerograd()
+        log_posterior.backward()
+
+        theta = update(theta, x, epoch)
+
+        theta1_all[epoch * n + i / batchsize] = theta[0]
+        theta2_all[epoch * n + i / batchsize] = theta[1]
         if i == 0:
-            print(epoch, theta1[0], theta2[0], theta1[0] * 2 + theta2[0])
+            print(epoch, theta, theta[0] * 2 + theta[1])
 
 H, xedges, yedges = numpy.histogram2d(theta1_all, theta2_all, bins=200)
 
