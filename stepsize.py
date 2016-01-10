d642e28538201a4547209107bf22acd4aa1ede7d
Kenta OONO <oono@preferred.jp>
2016-01-10 17:42:03 +0900

    separate stepsizegenerator

---
+++ stepsize.py
@@ -0,0 +1,30 @@
+
+class StepSizeGenerator(object):
+
+    def __init__(self, max_epoch, eps_start=0.01, eps_end=0.0001, gamma=0.55):
+        self.a, self.b = self.__calc_ab(eps_start, eps_end, gamma, max_epoch)
+        self.gamma = gamma
+
+    def __calc_ab(self, eps_start, eps_end, gamma, epoch):
+        """Returns coefficients that characterize step size
+
+        Args:
+            eps_start(float): initial step size
+            eps_end(float): initial step size
+            gamma(float): decay rate
+            epoch(int): # of epoch
+        Returns:
+            pair of float: (A, B) satisfies ``A / B ** gamma == eps_start``
+            and ``A / (B + epoch) ** gamma == eps_end``
+        """
+
+        B = 1 / ((eps_start / eps_end) ** (1 / gamma) - 1) * epoch
+        A = eps_start * B ** gamma
+        eps_start_actual = A / B ** gamma
+        eps_end_actual = A / (B + epoch) ** gamma
+        assert abs(eps_start - eps_start_actual) < 1e-4
+        assert abs(eps_end - eps_end_actual) < 1e-4
+        return A, B
+
+    def __call__(self, epoch):
+        return  self.a / (self.b + epoch) ** self.gamma
--- toy_sgld.py
+++ toy_sgld.py
@@ -12,36 +12,8 @@ import numpy
 import six
 
 import model
+import stepsize
 
-
-def calc_ab(eps_start, eps_end, gamma, epoch):
-    """Returns coefficients that characterize step size
-
-    Args:
-        eps_start(float): initial step size
-        eps_end(float): initial step size
-        gamma(float): decay rate
-        epoch(int): # of epoch
-    Returns:
-        pair of float: (A, B) satisfies ``A / B ** gamma == eps_start``
-        and ``A / (B + epoch) ** gamma == eps_end``
-    """
-
-    B = 1 / ((eps_start / eps_end) ** (1 / gamma) - 1) * epoch
-    A = eps_start * B ** gamma
-    eps_start_actual = A / B ** gamma
-    eps_end_actual = A / (B + epoch) ** gamma
-    assert abs(eps_start - eps_start_actual) < 1e-4
-    assert abs(eps_end - eps_end_actual) < 1e-4
-    return A, B
-
-
-# SGLD parameters
-EPS_START = 0.01
-EPS_END = 0.0001
-GAMMA = 0.55
-EPOCH = 10000
-A, B = calc_ab(EPS_START, EPS_END, GAMMA, EPOCH)
 
 # sample size
 n = 100
@@ -52,7 +24,7 @@ SEED = 0
 numpy.random.seed(SEED)
 
 
-def update(theta, x, epoch):
+def update(theta, x, epoch, eps):
     """One parameter-update step of SGLD
 
     Args:
@@ -64,19 +36,20 @@ def update(theta, x, epoch):
         same as theta
     """
     d_theta = model.calc_grad(theta, x)
-    eps = A / (B + epoch) ** GAMMA
     eta = numpy.random.randn() * numpy.sqrt(eps)
     return theta + d_theta * eps / 2 + eta
 
 
 theta1_all = numpy.empty((EPOCH * n_batch,), dtype=numpy.float32)
 theta2_all = numpy.empty((EPOCH * n_batch,), dtype=numpy.float32)
+ssg = stepsize.StepSizeGenerator(epoch=EPOCH)
 theta = model.sample_from_prior()
 x = model.generate(n)
 for epoch in six.moves.range(EPOCH):
     perm = numpy.random.permutation(n)
     for i in six.moves.range(0, n, batchsize):
-        theta = update(theta, x[perm][i: i+batchsize], epoch)
+        theta = update(theta, x[perm][i: i+batchsize],
+                       epoch, ssg(epoch))
 
         theta1_all[epoch * n_batch + i // batchsize] = theta[0]
         theta2_all[epoch * n_batch + i // batchsize] = theta[1]
