3381f25ced475a2d3c30f95cab9a14ae4245015c
Kenta OONO <oono@preferred.jp>
2016-01-11 02:42:20 +0900

    Fix parameter of SGHMC

--- toy_sghmc.py
+++ toy_sghmc.py
@@ -12,16 +12,21 @@ import stepsize
 
 
 parser = argparse.ArgumentParser(description='SGHMC')
+# true parameter
+parser.add_argument('--theta1', default=0, type=float, help='true paremter 1')
+parser.add_argument('--theta2', default=1, type=float, help='true paremter 2')
 # data
 parser.add_argument('--N', default=100, type=int, help='training data size')
-parser.add_argument('--batchsize', default=1, type=int, help='batchsize')
-parser.add_argument('--epoch', default=100, type=int, help='epoch num')
+parser.add_argument('--batchsize', default=10, type=int, help='batchsize')
+parser.add_argument('--epoch', default=1000, type=int, help='epoch num')
 # SGLMC parameter
 parser.add_argument('--F', default=30, type=float, help='friction parameter')
 parser.add_argument('--D', default=10, type=float, help='diffusion parameter')
 parser.add_argument('--initialize-moment', action='store_true',
                     help='If true, initialize moment in each sample')
 parser.add_argument('--L', default=10, type=int, help='sampling interval')
+parser.add_argument('--eps-start', default=0.01, type=float, help='start stepsize')
+parser.add_argument('--eps-end', default=0.005, type=float, help='end stepsize')
 # others
 parser.add_argument('--seed', default=0, type=int, help='random seed')
 parser.add_argument('--visualize', default='visualize_sghmc.png', type=str,
@@ -50,18 +55,17 @@ def update(p, theta, x, eps):
 
 theta1_all = numpy.empty((args.epoch * n_batch,), dtype=numpy.float32)
 theta2_all = numpy.empty((args.epoch * n_batch,), dtype=numpy.float32)
-ssg = stepsize.StepSizeGenerator(args.epoch)
+ssg = stepsize.StepSizeGenerator(args.epoch, args.eps_start, args.eps_end)
 theta = model.sample_from_prior()
 p = numpy.random.randn(*theta.shape)
-x = model.generate(args.N)
+x = model.generate(args.N, args.theta1, args.theta2)
 for epoch in six.moves.range(args.epoch):
     perm = numpy.random.permutation(args.N)
     for i in six.moves.range(0, args.N, args.batchsize):
         if args.initialize_moment:
             p = numpy.random.randn(*theta.shape)
-        eps = ssg(epoch)
         for l in six.moves.range(args.L):
-            p, theta = update(p, theta, x[perm][i: i + args.batchsize], eps)
+            p, theta = update(p, theta, x[perm][i: i + args.batchsize], ssg(epoch))
 
         theta1_all[epoch * n_batch + i // args.batchsize] = theta[0]
         theta2_all[epoch * n_batch + i // args.batchsize] = theta[1]
